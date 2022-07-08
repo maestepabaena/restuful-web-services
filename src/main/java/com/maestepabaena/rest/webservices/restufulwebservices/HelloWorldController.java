f984c2eab163dcd454b19ba6f25eeb71dc550281
maestepabaena <maestepabaena@gmail.com>
2022-07-08 12:49:16 +0200

    GetMapping

--- src/main/java/com/maestepabaena/rest/webservices/restufulwebservices/HelloWorldController.java
+++ src/main/java/com/maestepabaena/rest/webservices/restufulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.maestepabaena.rest.webservices.restufulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    // GET
-    // URI - /hello-world
-    //Method - "Hello World"
-
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
