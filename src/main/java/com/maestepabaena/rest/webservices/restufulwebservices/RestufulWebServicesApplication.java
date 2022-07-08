da595eacac747959f2706654ac51da99a0820ff3
maestepabaena <maestepabaena@gmail.com>
2022-07-08 12:27:22 +0200

    Initial commit

---
+++ src/main/java/com/maestepabaena/rest/webservices/restufulwebservices/RestufulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.maestepabaena.rest.webservices.restufulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestufulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestufulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maestepabaena/rest/webservices/restufulwebservices/RestufulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maestepabaena.rest.webservices.restufulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestufulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
